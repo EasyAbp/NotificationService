b4a04c489d27b39705e00d3791920859e8afd477
gdlcf88 <gdlcf88@outlook.com>
2020-12-22 19:47:07 +0800

    Add migrations

--- host/EasyAbp.NotificationService.Web.Unified/Migrations/20201222114522_AddedNotificationAndNotificationInfo.Designer.cs
+++ host/EasyAbp.NotificationService.Web.Unified/Migrations/20201222114522_AddedNotificationAndNotificationInfo.Designer.cs
@@ -4,15 +4,17 @@ using EasyAbp.NotificationService.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
 namespace EasyAbp.NotificationService.Migrations
 {
     [DbContext(typeof(UnifiedDbContext))]
-    partial class UnifiedDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201222114522_AddedNotificationAndNotificationInfo")]
+    partial class AddedNotificationAndNotificationInfo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ host/EasyAbp.NotificationService.Web.Unified/Migrations/20201222114522_AddedNotificationAndNotificationInfo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EasyAbp.NotificationService.Migrations
+{
+    public partial class AddedNotificationAndNotificationInfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NotificationServiceNotificationInfos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    ExtraProperties = table.Column<string>(nullable: true),
+                    ConcurrencyStamp = table.Column<string>(maxLength: 40, nullable: true),
+                    CreationTime = table.Column<DateTime>(nullable: false),
+                    CreatorId = table.Column<Guid>(nullable: true),
+                    LastModificationTime = table.Column<DateTime>(nullable: true),
+                    LastModifierId = table.Column<Guid>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false, defaultValue: false),
+                    DeleterId = table.Column<Guid>(nullable: true),
+                    DeletionTime = table.Column<DateTime>(nullable: true),
+                    TenantId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotificationServiceNotificationInfos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NotificationServiceNotifications",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    ExtraProperties = table.Column<string>(nullable: true),
+                    ConcurrencyStamp = table.Column<string>(maxLength: 40, nullable: true),
+                    CreationTime = table.Column<DateTime>(nullable: false),
+                    CreatorId = table.Column<Guid>(nullable: true),
+                    TenantId = table.Column<Guid>(nullable: true),
+                    UserId = table.Column<Guid>(nullable: false),
+                    NotificationInfoId = table.Column<Guid>(nullable: false),
+                    NotificationMethod = table.Column<string>(nullable: true),
+                    Success = table.Column<bool>(nullable: true),
+                    CompletionTime = table.Column<DateTime>(nullable: true),
+                    FailureReason = table.Column<string>(nullable: true),
+                    RetryNotificationId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotificationServiceNotifications", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NotificationServiceNotificationInfos");
+
+            migrationBuilder.DropTable(
+                name: "NotificationServiceNotifications");
+        }
+    }
+}
