6795c8ce414c05c33239c8f83927c2755338a353
Slarkerino Company Desktop <307221214@qq.com>
2021-07-14 13:49:30 +0800

    Remove Property Dictionary from CreatePrivateMessageNotificationEto

--- providers/PrivateMessaing/EasyAbp.NotificationService.Provider.PrivateMessaging.Abstrations/EasyAbp/NotificationService/Provider/PrivateMessaging/CreatePrivateMessageNotificationEto.cs
+++ providers/PrivateMessaing/EasyAbp.NotificationService.Provider.PrivateMessaging.Abstrations/EasyAbp/NotificationService/Provider/PrivateMessaging/CreatePrivateMessageNotificationEto.cs
@@ -14,32 +14,25 @@ namespace EasyAbp.NotificationService.Provider.PrivateMessaging
         [CanBeNull]
         public string Content { get; set; }
 
-        [NotNull]
-        public IDictionary<string, object> Properties { get; protected set; }
-
 
         public CreatePrivateMessageNotificationEto(
             Guid? tenantId,
             IEnumerable<Guid> userIds,
             [NotNull] string title,
-            [CanBeNull] string content,
-            [NotNull] IDictionary<string, object> properties) : base(tenantId, userIds)
+            [CanBeNull] string content) : base(tenantId, userIds)
         {
             Title = title;
             Content = content;
-            Properties = properties;
         }
         
         public CreatePrivateMessageNotificationEto(
             Guid? tenantId,
             Guid userId,
             [NotNull] string title,
-            [CanBeNull] string content,
-            [NotNull] IDictionary<string, object> properties) : base(tenantId, userId)
+            [CanBeNull] string content) : base(tenantId, userId)
         {
             Title = title;
             Content = content;
-            Properties = properties;
         }
     }
 }
